6c585791b89cea3ae198679ac0acf761a6a37a47
Chinawut Sangkheetpakorn <csangkhe@z3t9c1.42bangkok.com>
2022-02-23 13:26:50 +0700

    23FEB2022

---
+++ ft_memmove.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csangkhe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 12:10:49 by csangkhe          #+#    #+#             */
+/*   Updated: 2022/02/23 12:30:44 by csangkhe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*dst_temp;
+	char	*src_temp;
+
+	dst_temp = (char *)dst;
+	src_temp = (char *)src;
+	if (dst == src)
+		return (dst);
+	else if (src_temp < dst_temp)
+	{
+		while (len--)
+			*(dst_temp + len) = *(src_temp + len);
+		return (dst);
+	}
+	while (len--)
+		*dst_temp++ = *src_temp++;
+	return (dst);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csangkhe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 11:24:53 by csangkhe          #+#    #+#             */
+/*   Updated: 2022/02/23 12:08:13 by csangkhe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	total;
+	size_t	dst_in;
+	size_t	src_in;
+	
+	dst_in = ft_strlen(dst);
+	total = dst_in + ft_strlen(src);
+	src_in = 0;
+	if (dst_in + 1 > dstsize)
+		return (total - dst_in + dstsize);
+	while (src[src_in] != '\0' && dst_in + 1 < dstsize)
+	{
+		dst[dst_in] = src[src_in];
+		dst_in++;
+		src_in++;
+	}
+	dst[dst_in] = '\0';
+	return (total);
+}
